117ef203dc4b80ea0e3dbe942b3258511f511f16
Leon <leonaviev@gmail.com>
2024-09-19 20:48:37 +0700

    Deployment: fix production issues

--- components/index.ts
+++ components/index.ts
@@ -3,6 +3,5 @@ import Navbar from "./Navbar";
 import LeftSidebar from "./LeftSidebar";
 import RightSidebar from "./RightSidebar";
 import CursorChat from "./cursor/CursorChat";
-import Live from "./Live";
 
-export { LiveCursors, Navbar, LeftSidebar, RightSidebar, CursorChat, Live };
+export { LiveCursors, Navbar, LeftSidebar, RightSidebar, CursorChat };
